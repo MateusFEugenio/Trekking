21b647e954688df74277e6a9622ef2d82663103e
Wesley Cantarino <32984446+wesley-cantarino@users.noreply.github.com>
2022-06-25 18:27:57 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,17 +1,51 @@
+#lib
 import cv2
 
-imagem = cv2.imread('pylon.jpg')
-classificador = cv2.CascadeClassifier('cascade.xml')
+#%%
 
-deteccoes = classificador.detecMultiScale(imagemcinza, scaleFactor=1.09,minNeighbors=5,
-minSize=(30,30),maxSize=(40,40))
+# funcao principal de detecção 
+def detectar_cone(img, print_img):
 
-print(deteccoes)
-print(len(detccoes))
+    #classificador = cv2.CascadeClassifier('cascade.xml')
+    classificador = cv2.CascadeClassifier('cascade10.xml')
 
-for(x, y,l, a) in deteccoes:
-    cv2.rectangle(imagem, (x, y),(x + l, y + a)(0,255,0),2)
+    imagemcinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('Detector de faces', imagem)
-    cv2.waitkey(0)
-    cv2.destroyallwindos( )
+    deteccoes = classificador.detectMultiScale(imagemcinza, scaleFactor = 1.1, minNeighbors = 4, flags=4, minSize = (50, 150))
+    
+    print(deteccoes)
+    if len(deteccoes) > 0:
+        print(f"\nFoi(Foram) detectado {len(deteccoes)} elemento(s)\n")
+    
+        x = deteccoes[:, 0]
+        y = deteccoes[:, 1]
+        l = deteccoes[:, 2]
+        a = deteccoes[:, 3]
+        
+        #detectar o maior valor de x
+        err_la = 0
+        aux_i = 0
+        
+        for i in range(len(x)):
+            if (err_la < (l[i] * a[i])):
+                err_la = l[i] * a[i]
+                aux_i = i
+        
+        err_la = x[aux_i] - len(img[0])/2
+        
+        if print_img:
+            for(x, y,l, a) in deteccoes:
+                cv2.rectangle(img, (x, y),(x + l, y + a), (0,255,0), 2)
+                cv2.imshow('Detector de faces', imagem)
+
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+        
+        return err_la
+        
+
+imagem = cv2.imread('cone.jpg')
+#imagem = cv2.imread('pylon.jpg')
+    
+err = detectar_cone(imagem, print_img = False)
+print(f"erro: {err}")
